517d3ea5eb4d00c94c75cc31a8ddabdf9797536d
JaredDelvalle1997 <34875141+JaredDelvalle1997@users.noreply.github.com>
2019-01-31 14:13:46 -0500

    Update robo_advisor.py

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,1 +1,12 @@
-print("hello World")
+print("Hello Welcome to the Retirement Robo-Adviser!")
+print("We hope you find the application useful!")
+
+cont = input("Would you like to begin -- y/n: ")
+
+# .lower() will make the user input uniform -- simple input issue solve
+while( cont.lower() == y):
+  #initialize the user for their age, used later in conditionals 
+  age = input("What is your age: ")
+  
+  #income?
+  #conditional statements here 
